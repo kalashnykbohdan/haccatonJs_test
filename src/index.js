0a6697c7bec46358836acb50e06aa4397479f48a
Kalashnyk Bohdan <space.bohdan@gmail.com>
2020-08-09 11:35:09 +0300

    first commit5

--- src/index.js
+++ src/index.js
@@ -8,18 +8,3 @@ import './js/components/2searchAndPlaginationHomePage.js';
 import './js/components/3navigation.js';
 import './js/components/4filmDetailsPage.js';
 import './js/components/5libraryPage.js';
-
-// const Header = document.querySelector('#header');
-
-// const markupHeader = markupHeaderFunc();
-// Header.insertAdjacentHTML("afterbegin", markupHeader);
-
-// function markupHeaderFunc() {
-
-//     const markup = `
-//     <h1>Привет</h1>
-//     <h1>Привет</h1>
-//     `
-//     return markup;
-// }
-// div.insertAdjacentHTML('beforebegin', '<p>Привет</p>');
